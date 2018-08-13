34b3bd3923b1ee0525f5f89378ff75afde176968
qinzheng_wang <qinzheng_wang@ziguangcn.com>
2018-08-13 17:11:05 +0800

    增加react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-let name = "ziguang";
-window.console.log(`hello, ${name}`);
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+import UserInfo from './pages/user/info/index'
+
+render(
+    <UserInfo></UserInfo>,
+    document.getElementById('root')
+)
--- src/pages/user/info/index.js
+++ src/pages/user/info/index.js
@@ -1,1 +1,14 @@
+import React, { Component } from 'react'
 import "./style.scss";
+
+class UserInfo extends Component {
+    render() {
+        return (
+            <div className="test">
+                user/info
+            </div>
+        )
+    }
+}
+
+export default UserInfo
