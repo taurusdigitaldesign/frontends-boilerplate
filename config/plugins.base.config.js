9e2e51bdda9fdf510682974d123e58f87280b17d
ming_lin <ming_lin@ziguangcn.com>
2018-12-15 15:14:25 +0800

    dir路径错误导致无法运行

--- config/plugins.base.config.js
+++ config/plugins.base.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
-const dirs = require('./base/dirs.js')
+const dirs = require('./base/dirs.config.js')
 const pages = require('./base/pages.js')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
