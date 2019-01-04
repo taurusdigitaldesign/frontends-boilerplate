a5a60e421710fcd156fe1c3d18b2f7bd322a83fc
qinzheng_wang <qinzheng_wang@ziguangcn.com>
2019-01-04 18:37:55 +0800

    正式环境配置移到config文件夹

---
+++ config/webpack.prod.config.js
@@ -0,0 +1,77 @@
+const base = require('./config/webpack.base.config')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const plugins = require('./config/plugins.prod.config')
+const dirs = require('./config/base/dirs');
+
+const config = {
+  ...base,
+  mode: 'production',
+  module: {
+      rules: [
+          {
+              enforce: 'pre',
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'eslint-loader',
+          },
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader',
+          },
+          {
+              test: /\.css$/,
+              loader: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: [{
+                      loader: 'css-loader'
+                  }]
+              })
+          },
+          {
+              test: /\.scss$/,
+              include: dirs.src,
+              loader: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: [
+                      {
+                          loader: 'css-loader',
+                          options: {
+                              module: true
+                          }
+                      },
+                      {
+                          loader: 'sass-loader'
+                      }
+                  ]
+              })
+          },
+          {
+              test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+              use: [
+                  {
+                      loader: 'url-loader',
+                      options: {
+                          limit: 5000,
+                          name: 'images/[name].[ext]'
+                      }
+                  }
+              ]
+          }
+      ]
+  },
+  optimization: {
+      splitChunks: {
+          cacheGroups: {
+              vendor: {
+                  test: /[\\/]node_modules[\\/]/,
+                  name: 'common',
+                  chunks: 'all'
+              }
+          }
+      }
+  },
+  plugins: plugins
+};
+
+module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,13 @@
-const base = require('./config/webpack.base.config')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const plugins = require('./config/plugins.prod.config')
-const dirs = require('./config/base/dirs');
-
 const argv = require('yargs').argv;
 const devConfig = require('./config/webpack.dev.config');
+const prodConfig = require('./config/webpack.prod.config');
 
-const config = {
-    ...base,
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [{
-                        loader: 'css-loader'
-                    }]
-                })
-            },
-            {
-                test: /\.scss$/,
-                include: dirs.src,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                module: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader'
-                        }
-                    ]
-                })
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 5000,
-                            name: 'images/[name].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'common',
-                    chunks: 'all'
-                }
-            }
-        }
-    },
-    plugins: plugins
-};
 
 switch (argv['env']) {
     case 'dev':
         module.exports = devConfig;
         break;
     default:
-        module.exports = config;
+        module.exports = prodConfig;
         break;
 }
