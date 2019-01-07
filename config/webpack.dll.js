1ac490fd884ea1e3e045f7d6b883f3377b5a9ce2
qinzheng_wang <qinzheng_wang@ziguangcn.com>
2019-01-07 10:02:30 +0800

    webpack配置移到config文件夹中

---
+++ config/webpack.dll.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const webpack = require('webpack')
+const dirs = require('./base/dirs')
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        vendor: ['react', 'react-dom', 'react-router-dom']
+    },
+    // devtool: '#source-map',
+    output: {
+        path: path.resolve(dirs.root, 'vendor'),
+        filename: '[name].dll.js',
+        library: '[name]'
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            name: 'vendor',
+            // 本Dll文件中各模块的索引，供DllReferencePlugin读取使用
+            path: path.resolve(dirs.root, 'vendor/manifest.json')
+        })
+    ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,2 @@
 const argv = require('yargs').argv;
-const devConfig = require('./config/webpack.dev.config');
-const prodConfig = require('./config/webpack.prod.config');
-
-
-switch (argv['env']) {
-    case 'dev':
-        module.exports = devConfig;
-        break;
-    default:
-        module.exports = prodConfig;
-        break;
-}
+module.exports = require(`./config/webpack.${argv['env']}`);
